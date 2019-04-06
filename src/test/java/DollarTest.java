8230ec49f0d185c8baeb4c45c6d750fc7bac3688
Maddie <beagleymaddie@gmail.com>
2019-04-07 10:41:45 +1200

    added incorrect assert to observe failing build

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,15 +6,13 @@ public class DollarTest {
 
     @Test
     public void testDollarDivision() {
-        System.out.println("Running first test");
         Dollar ten = new Dollar(10);
         Dollar result = ten.dividedBy(2);
-        assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
+        assertEquals(new Dollar(4), result); // You could change 5 to 4 to see the build failure
     }
 
     @Test(expected=ArithmeticException.class)
     public void testDollarDividedByZero() {
-        System.out.println("Running second test");
         Dollar ten = new Dollar(10);
         ten.dividedBy(0);
     }
