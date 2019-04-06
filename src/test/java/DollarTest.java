478672ff0925111b712e1d831c280a587e1517a0
Maddie <beagleymaddie@gmail.com>
2019-04-07 10:35:59 +1200

    added print statements to test

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -6,6 +6,7 @@ public class DollarTest {
 
     @Test
     public void testDollarDivision() {
+        System.out.println("Running first test");
         Dollar ten = new Dollar(10);
         Dollar result = ten.dividedBy(2);
         assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
@@ -13,6 +14,7 @@ public class DollarTest {
 
     @Test(expected=ArithmeticException.class)
     public void testDollarDividedByZero() {
+        System.out.println("Running second test");
         Dollar ten = new Dollar(10);
         ten.dividedBy(0);
     }
