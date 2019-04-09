e006908edd87a4cb54adbb55e9eebde3f6624357
Maddie <beagleymaddie@gmail.com>
2019-04-09 14:00:52 +1200

    Removed a test to reduce code coverage

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -11,10 +11,4 @@ public class DollarTest {
         assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
     }
 
-    @Test(expected=ArithmeticException.class)
-    public void testDollarDividedByZero() {
-        Dollar ten = new Dollar(10);
-        ten.dividedBy(0);
-    }
-
 }
